dbcebc2b0b22594a0cc3c61e4e5b860bc6ba79a1
Wenjie Wang <wenjie.2.wang@uconn.edu>
2017-01-07 15:40:44 -0500

    take care of grid

--- R/bayesCox.R
+++ R/bayesCox.R
@@ -35,15 +35,6 @@
 ##' model. The baseline hazards are allowed to be either time-varying or
 ##' dynamic.
 ##'
-##' For application, it is recommended that users preprocess the data by
-##' rounding down (up) the left (right) endpoints of the censoring intervals
-##' to at most three significant digits and leave the \code{grid} unspecified
-##' in the function call to reduce probably unnecessary computational burden.
-##' It also helps avoid possible errors caused by mispecifed \code{grid}. For
-##' example, the left endpoints can be rounded down in the nearest 0.1 unit
-##' of time by \code{left <- floor(left * 10) / 10} and the right endpoints
-##' can similarly be rounded up by \code{right <- ceiling(right * 10) / 10}.
-##'
 ##' To use default hyper parameters in the specification of either
 ##' \code{base.prior} or \code{coef.prior}, one only has to supply the name of
 ##' the prior, e.g., \code{list(type = "Gamma")}, \code{list(type = "HAR1")}.
@@ -86,7 +77,7 @@
 ##' object returned by the function.}
 ##'     \item{Part 2:}{The sequence from \eqn{(k + 1) to (k + ck * p)}
 ##' represent the coefficients of covariates at the time grid.  The first
-##' \eqn{k} numbers in the sequence are the coefficients for the first covaraite
+##' \eqn{k} numbers in the sequence are the coefficients for the first covariate
 ##' at the time grid; The second \eqn{k} numbers' sub-sequence are the
 ##' coefficients for the second covariate and so on.}
 ##'     \item{Part 3:}{The sequence from \eqn{(k + ck * p + 1)} to
@@ -117,15 +108,19 @@
 ##'
 ##' @param formula A formula object, with the response on the left of a '~'
 ##'     operator, and the terms on the right. The response must be a survival
-##'     object as returned by the \code{Surv} function.
+##'     object as returned by the function \code{Surv} with \code{type =
+##'     "interval2"}. \code{help(Surv)} for details.
 ##' @param data A data.frame in which to interpret the variables named in the
 ##'     \code{formula}.
 ##' @param grid Vector of pre-specified time grid points for model fitting.
 ##'     It will be automatically set up from data if it is left unspecified
 ##'     in the function call. By default, it consists of all the unique
-##'     finite endpoints of the censoring intervals after time zero.  The
-##'     \code{grid} specified in the function call must be sorted, and covers
-##'     all the finite non-zero endpoints of the censoring intervals.
+##'     finite endpoints (rounded to two significant numbers) of the
+##'     censoring intervals after time zero.  The \code{grid} specified in
+##'     the function call determines the location of possible jumps. It should
+##'     be sorted increasingly and cover all the finite non-zero endpoints of
+##'     the censoring intervals. Inappropriate \code{grid} specified will be
+##'     taken care by the function internally.
 ##' @param out Name of Markov chain Monte Carlo (MCMC) samples output file.
 ##'     Each row contains one MCMC sample information. The file is needed for
 ##'     those functions further summarizing estimation results in this
@@ -180,7 +175,7 @@
 ##' ### breast cancer data
 ##' data(bcos)       # attach bcos and bcos.grid
 ##' mydata <- bcos
-##' ## mygrid <- bcos.grid
+##' mygrid <- bcos.grid
 ##' myformula <- Surv(left, right, type = "interval2") ~ trt
 ##'
 ##' ### tooth data
@@ -200,7 +195,11 @@
 ##'                  gibbs = list(iter = 100, burn = 20, thin = 1,
 ##'                               verbose = TRUE, nReport = 20))
 ##' plotCoef(coef(fit0, level = 0.9))
-##' plotSurv(survCurve(fit0), conf.int = TRUE)
+##'
+##' ## Plot the estimated survival function for given new data
+##' newDat <- data.frame(trt = c("Rad", "RadChem"))
+##' row.names(newDat) <- unique(newDat$trt)
+##' plotSurv(survCurve(fit0, newDat), conf.int = TRUE)
 ##'
 ##' ## Fit time-varying coefficient model
 ##' fit1 <- bayesCox(myformula, mydata, out = "tvCox.txt", model = "TimeVary",
@@ -227,12 +226,11 @@
 ##' plotCoef(rbind(coef(fit0), coef(fit1), coef(fit2)))
 ##'
 ##' ## Fit dynamic coefficient model with dynamic hazards (in log scales)
-##' fit3 <- bayesCox(myformula, mydata, out = "dynCox2.txt",
-##'                  model = "Dynamic",
+##' fit3 <- bayesCox(myformula, mydata, out = "dynCox2.txt", model = "Dynamic",
 ##'                  base.prior = list(type = "Const"),
 ##'                  coef.prior = list(type = "HAR1", shape = 2, scale = 1),
 ##'                  gibbs = list(iter = 100, burn = 20, thin = 1,
-##'                               verbose = TRUE, nReport=5),
+##'                               verbose = TRUE, nReport = 20),
 ##'                  control = list(intercept = TRUE))
 ##' plotCoef(coef(fit3))
 ##' plotJumpTrace(jump(fit3))
@@ -240,15 +238,13 @@
 ##' plotNu(nu(fit3))
 ##' plotSurv(survCurve(fit3), conf.int = TRUE)
 ##'
-##' ## Plot the estimated survival function for given new data
-##' newDat <- data.frame(trt = c("Rad", "RadChem"))
-##' row.names(newDat) <- unique(newDat$trt)
+##' ## Plot the estimated survival function and the difference
 ##' plotSurv(survCurve(fit3, newdata = newDat, type = "survival"),
 ##'          legendName = "Treatment", conf.int = TRUE)
 ##' plotSurv(survDiff(fit3, newdata = newDat, type = "survival"),
 ##'          legendName = "Treatment", conf.int = TRUE, smooth = TRUE)
 ##' }
-##' @importFrom stats model.frame model.matrix .getXlevels
+##' @importFrom stats model.frame model.matrix .getXlevels stepfun
 ##' @importFrom utils tail
 ##' @export bayesCox
 bayesCox <- function(formula, data, grid = NULL, out = "mcmc.txt",
@@ -268,8 +264,7 @@ bayesCox <- function(formula, data, grid = NULL, out = "mcmc.txt",
         if (base.prior$type == "GammaProcess" &&
             coef.prior$type == "Normal")
             id <- 12
-    }
-    if (model == "TimeVarying") {
+    } else if (model == "TimeVarying") {
         if (base.prior$type == "Gamma" && coef.prior$type == "AR1")
             id <- 21
         if (base.prior$type == "Gamma" && coef.prior$type == "HAR1")
@@ -280,9 +275,7 @@ bayesCox <- function(formula, data, grid = NULL, out = "mcmc.txt",
         if (base.prior$type == "GammaProcess" &&
             coef.prior$type == "HAR1")
             id <- 24
-
-    }
-    if (model == "Dynamic") {
+    } else if (model == "Dynamic") {
         if (base.prior$type == "Gamma" && coef.prior$type == "AR1")
             id <- 31
         if (base.prior$type == "Gamma" && coef.prior$type == "HAR1")
@@ -297,7 +290,8 @@ bayesCox <- function(formula, data, grid = NULL, out = "mcmc.txt",
         if (base.prior$type == "GammaProcess" &&
             coef.prior$type == "HAR1")
             id <- 36
-    }
+    } else
+        stop("Invalid 'model' specified.")
 
     gibbs <- do.call("gibbs_fun", gibbs)
     control <- do.call("control_bfun", control)
@@ -307,14 +301,26 @@ bayesCox <- function(formula, data, grid = NULL, out = "mcmc.txt",
     mt <- attr(mf, "terms")
     mm <- model.matrix(formula, data)
 
-    LRX <- cbind(mf[, 1][, 1:2], mm[, -1])
-    obsInd <- which(mf[, 1][, 3] == 1)
-    LRX[obsInd, 2] <- LRX[obsInd, 1]
+    LRX <- cbind(mf[, 1L][, seq_len(2L)], mm[, - 1L])
 
-    cov.names <- colnames(mm)[-1]
+    ## reference: Surv.S from survival package
+    eventIdx <- mf[, 1L][, 3L]
+    ## for possibly - Inf or NA left
+    tmpIdx <- eventIdx == 2
+    LRX[tmpIdx, 1L] <- 0
+    ## for exact event times
+    tmpIdx <- eventIdx == 1
+    LRX[tmpIdx, 2L] <- LRX[tmpIdx, 1L]
+    ## for right censoring
+    tmpIdx <- eventIdx == 0
+    LRX[tmpIdx, 2L] <- Inf
 
-    ## prepare the grid if it is not specified
+    ## record coveriate names
+    cov.names <- colnames(mm)[- 1L]
+
+    ## take care of grid
     if (is.null(grid)) {
+        ## prepare the grid if it is not specified
         ## determine the significant digits from the data
         charNum <- as.character(LRX[, seq_len(2)])
         tmpList <- strsplit(charNum, "\\.")
@@ -323,12 +329,8 @@ bayesCox <- function(formula, data, grid = NULL, out = "mcmc.txt",
                 return(nchar(a[2L]))
             0
         }))
-        if (sigMax > 3)
-            warning(paste("Endpoints of censoring intervals were enlarged",
-                          "to three significant digits\n to reduce",
-                          "computational burden."))
         ## round the right (left) endpoints up (down)
-        roundPow <- 10 ^ min(sigMax, 3)
+        roundPow <- 10 ^ min(sigMax, 2)
         left_ <- floor(LRX[, "time1"] * roundPow) / roundPow
         right_ <- ceiling(LRX[, "time2"] * roundPow) / roundPow
         finiteRight <- right_[! (is.infinite(right_) | is.na(right_))]
@@ -339,16 +341,35 @@ bayesCox <- function(formula, data, grid = NULL, out = "mcmc.txt",
 
         LRX[, "time1"] <- left_
         LRX[, "time2"] <- right_
+    } else {
+        ## make sure all grid points great than zero, finite and sorted
+        if (any(tmpIdx <- is.na(grid) | is.infinite(grid)))
+            grid <- grid[! tmpIdx]
+        if (any(tmpIdx <- grid <= 0))
+            grid <- sort(unique(grid[! tmpIdx]))
+        if (all(tmpIdx <- is.infinite(LRX[, "time2"])))
+            stop("Subjects are all right censored.")
+        finiteRight <- max(LRX[! tmpIdx, "time2"])
+        if (tail(grid, 1L) < finiteRight) {
+            warning(paste("grid' was expanded to cover all the finite endpoint",
+                          "of censoring intervals."))
+            grid <- c(grid, finiteRight)
+        }
+        ## round the left (right) endpoints down (up)
+        ## to the closest grid point including zero
+        toLeft <- stats::stepfun(grid, c(0, grid))
+        toRight <- stats::stepfun(grid, c(grid, Inf))
+        LRX[, "time1"] <- toLeft(LRX[, "time1"])
+        LRX[, "time2"] <- toRight(LRX[, "time2"])
     }
 
-    LRX[LRX[, 2] == Inf, 2] <- max(tail(grid, 1), 999)
-    LRX[is.na(LRX[, 2]), 2] <- max(tail(grid, 1), 999)
+    LRX[is.infinite(LRX[, 2L]), 2L] <- max(tail(grid, 1L), 999)
+    LRX[is.na(LRX[, 2L]), 2L] <- max(tail(grid, 1L), 999)
 
     if (control$intercept) {
-        LRX <- cbind(LRX[, 1:2], 1, LRX[, -c(1:2)])
+        LRX <- cbind(LRX[, seq_len(2L)], 1, LRX[, - seq_len(2L)])
         cov.names <- c("intercept", cov.names)
     }
-
     colnames(LRX) <- c("L", "R", cov.names)
 
     ## Prepare results holder
