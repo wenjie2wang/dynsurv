dcc1b97ea1e02eccace5afd7470eb23077b15ac4
Wenjie Wang <wjwang.stat@gmail.com>
2018-03-22 23:19:23 -0400

    update copyright year and increase to dev version

--- R/dynsurv-package.R
+++ R/dynsurv-package.R
@@ -1,7 +1,7 @@
 ################################################################################
 ##
 ##   R package dynsurv by Wenjie Wang, Ming-Hui Chen, Xiaojing Wang, and Jun Yan
-##   Copyright (C) 2011-2017
+##   Copyright (C) 2011-2018
 ##
 ##   This file is part of the R package dynsurv.
 ##
