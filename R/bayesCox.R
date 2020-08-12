75e342424dc4a78c9de4e681e5248c7607ba25ff
Wenjie Wang <wjwang.stat@gmail.com>
2020-08-12 11:51:56 -0400

    move examples to inst

--- R/bayesCox.R
+++ R/bayesCox.R
@@ -163,90 +163,12 @@
 ##' selection for interval-censored survival data. \emph{Biometrics} 55(2),
 ##' 585--590.
 ##' @keywords Bayesian Cox dynamic interval censor
-##' @examples
-##' \dontrun{
-##' library(dynsurv)
-##' set.seed(1216)
+##' @example inst/examples/ex-bayesCox.R
 ##'
-##' ############################################################################
-##' ### Attach one of the following two data sets
-##' ############################################################################
-##'
-##' ### breast cancer data
-##' data(bcos)       # attach bcos and bcos.grid
-##' mydata <- bcos
-##' mygrid <- bcos.grid
-##' myformula <- Surv(left, right, type = "interval2") ~ trt
-##'
-##' ### tooth data
-##' ## data(tooth)   # attach tooth and tooth.grid
-##' ## mydata <- tooth
-##' ## mygrid <- tooth.grid
-##' ## myformula <- Surv(left, rightInf, type = "interval2") ~ dmf + sex
-##'
-##' ############################################################################
-##' ### Fit Bayesian Cox models
-##' ############################################################################
-##'
-##' ## Fit time-independent coefficient model
-##' fit0 <- bayesCox(myformula, mydata, out = "tiCox.txt", model = "TimeIndep",
-##'                  base.prior = list(type = "Gamma", shape = 0.1, rate = 0.1),
-##'                  coef.prior = list(type = "Normal", mean = 0, sd = 1),
-##'                  gibbs = list(iter = 100, burn = 20, thin = 1,
-##'                               verbose = TRUE, nReport = 20))
-##' plotCoef(coef(fit0, level = 0.9))
-##'
-##' ## Plot the estimated survival function for given new data
-##' newDat <- data.frame(trt = c("Rad", "RadChem"))
-##' row.names(newDat) <- unique(newDat$trt)
-##' plotSurv(survCurve(fit0, newDat), conf.int = TRUE)
-##'
-##' ## Fit time-varying coefficient model
-##' fit1 <- bayesCox(myformula, mydata, out = "tvCox.txt", model = "TimeVary",
-##'                  base.prior = list(type = "Gamma", shape = 0.1, rate = 0.1),
-##'                  coef.prior = list(type = "AR1", sd = 1),
-##'                  gibbs = list(iter = 100, burn = 20, thin = 1,
-##'                               verbose = TRUE, nReport = 20))
-##' plotCoef(coef(fit1))
-##' plotSurv(survCurve(fit1), conf.int = TRUE)
-##'
-##' ## Fit dynamic coefficient model with time-varying baseline hazards
-##' fit2 <- bayesCox(myformula, mydata, out = "dynCox1.txt", model = "Dynamic",
-##'                  base.prior = list(type = "Gamma", shape = 0.1, rate = 0.1),
-##'                  coef.prior = list(type = "HAR1", shape = 2, scale = 1),
-##'                  gibbs = list(iter = 100, burn = 20, thin = 1,
-##'                               verbose = TRUE, nReport = 20))
-##' plotCoef(coef(fit2))
-##' plotJumpTrace(jump(fit2))
-##' plotJumpHist(jump(fit2))
-##' plotNu(nu(fit2))
-##' plotSurv(survCurve(fit2), conf.int = TRUE)
-##'
-##' ## Plot the coefficient estimates from three models together
-##' plotCoef(rbind(coef(fit0), coef(fit1), coef(fit2)))
-##'
-##' ## Fit dynamic coefficient model with dynamic hazards (in log scales)
-##' fit3 <- bayesCox(myformula, mydata, out = "dynCox2.txt", model = "Dynamic",
-##'                  base.prior = list(type = "Const"),
-##'                  coef.prior = list(type = "HAR1", shape = 2, scale = 1),
-##'                  gibbs = list(iter = 100, burn = 20, thin = 1,
-##'                               verbose = TRUE, nReport = 20),
-##'                  control = list(intercept = TRUE))
-##' plotCoef(coef(fit3))
-##' plotJumpTrace(jump(fit3))
-##' plotJumpHist(jump(fit3))
-##' plotNu(nu(fit3))
-##' plotSurv(survCurve(fit3), conf.int = TRUE)
-##'
-##' ## Plot the estimated survival function and the difference
-##' plotSurv(survCurve(fit3, newdata = newDat, type = "survival"),
-##'          legendName = "Treatment", conf.int = TRUE)
-##' plotSurv(survDiff(fit3, newdata = newDat, type = "survival"),
-##'          legendName = "Treatment", conf.int = TRUE, smooth = TRUE)
-##' }
 ##' @importFrom stats model.frame model.matrix .getXlevels stepfun
 ##' @importFrom utils tail
-##' @export bayesCox
+##'
+##' @export
 bayesCox <- function(formula, data, grid = NULL, out = "mcmc.txt",
                      model = c("TimeIndep", "TimeVarying", "Dynamic"),
                      base.prior = list(), coef.prior = list(), gibbs = list(),
@@ -351,8 +273,8 @@ bayesCox <- function(formula, data, grid = NULL, out = "mcmc.txt",
         stop("Subjects are all right censored.")
     finiteRight <- max(LRX[! tmpIdx, "time2"])
     if (tail(grid, 1L) < finiteRight) {
-        warning(paste("The grid was expanded to cover all the finite endpoint",
-                      "of censoring intervals."))
+        warning("The grid was expanded to cover all the finite endpoint",
+                "of censoring intervals.")
         grid <- c(grid, finiteRight)
     }
     ## round the left (right) endpoints down (up)
