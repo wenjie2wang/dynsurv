c18e7bd8e55333bb84fa6bf1edd82011830003e8
Wenjie Wang <wang@wwenjie.org>
2022-02-15 19:32:57 -0500

    Bump copyright year

--- R/bayesCoxMcmc.R
+++ R/bayesCoxMcmc.R
@@ -1,6 +1,6 @@
 ##
 ## R package dynsurv by Wenjie Wang, Ming-Hui Chen, Xiaojing Wang, and Jun Yan
-## Copyright (C) 2011-2020
+## Copyright (C) 2011-2022
 ##
 ## This file is part of the R package dynsurv.
 ##
